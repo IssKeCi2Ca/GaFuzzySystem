82686246d01a70296b92990cb5f44a24f9411f09
Chee Kit <kookit@gmail.com>
2018-10-08 23:45:26 +0800

    10 rules per individual, 20 individuals

--- GA.py
+++ GA.py
@@ -4,9 +4,9 @@ Created on Mon Oct  8 01:08:18 2018
 
 @author: kooc
 """
+# pylint: disable=no-member
 
 import random
-import pprint
 
 from deap import base
 from deap import creator
@@ -21,8 +21,8 @@ MNValues_MIN, MNValues_MAX = 1, 32
 FuzzyExtent = ['EL', 'VL', 'L', 'M', 'H', 'VH', 'EH']
 Recommend_MIN, Recommend_MAX = -10, 10
 
-N_CYCLES = 1
-POP_SIZE = 10
+N_CYCLES = 10
+POP_SIZE = 20
 
 toolbox.register("attr_mamethod", random.choice, MAMethod)
 toolbox.register("attr_mnvalues", random.randint, MNValues_MIN, MNValues_MAX)
@@ -34,8 +34,11 @@ toolbox.register("individual", tools.initCycle, creator.Individual,
                   toolbox.attr_fuzzzyextent, toolbox.attr_recommend), n=N_CYCLES)
 
 # ind = toolbox.individual()
+# print(ind)
+
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 pop = toolbox.population(POP_SIZE)
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(pop)
+# printing the list using loop 
+for x in range(POP_SIZE): 
+   print (pop[x])
