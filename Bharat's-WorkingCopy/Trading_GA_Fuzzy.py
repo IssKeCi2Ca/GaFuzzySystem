e2da22033860b735c30bd2aea250b9e0d8b860af
rajbharat <8273173+rajbharat@users.noreply.github.com>
2018-10-20 16:08:32 +0800

    Add files via upload

--- Bharat's-WorkingCopy/Trading_GA_Fuzzy.py
+++ Bharat's-WorkingCopy/Trading_GA_Fuzzy.py
@@ -1,125 +1,103 @@
 import numpy as np #matrix math
-# written in a separate file ga
-import ga
-#Fuzzy Logic Code Goes in here
-import AssetFuzzy2
 import pandas as pd
 from datetime import datetime
 from datetime import date
 from datetime import time
 from datetime import timedelta
+import AssetFuzzy4
+import ga
 
 #Implementation of Trading System - Updating Stocks,Cash Balance
 class Trading:
-        #Program divided into 4 parts.
-        #1) Initialise the required variables
-        #2) Define Helper Functions such as Update Date Time, Audit Date Changes, Download Audit Report
-        #3) Implement GA & Fuzzy logic in Training and Trading Methods
-        #4) Initialise the required variables
-################################################################# 1. Initialise the required dataframes and variables ##########################################################        
 	def __init__(self):
-                #initialise other variables SystemDate, Trading Reqd
-		initMarketRates()
-		#load our GA and Fuzzy Models
-		Training_Done= 0
-		Trading_Required=0
-		System_Date = datetime.datetime(2011, 1, 1, 00, 00)
-		#year, month, date, hh, mm
-		popSize = 10
-		i=0
-		#pop = ga.GA.Generate_Random_Individuals(popSize)
+		self.Training_Done= 0
+		self.Trading_Required=0
+		self.System_Date = datetime.datetime(2011, 1, 1, 00, 00)
+		self.popSize = 10
+		self.i=0
+		self.cash_bal=10000000
+		self.TradingInfo = pd.DataFrame()#to hold back testing results
+		self.BackTestingInfo = pd.DataFrame()#to hold back testing results
+		self.train_dict = None
+		self.test_dict = None
+		self.FCPO = None
+		self.initMarketRates()
 
-################################################################# 2.Helper Functions (Start) ###################################################################################
-
-        #Load the Rates in a Pandas Frame. All the Data Given can be converted to a CSV And imported into a frame
-	def initMarketRates():
-                #All the Data Given can be converted to a CSV And imported into a frame
-                # Read in the initial database 2011-16.  Timenow 2014
-                FCPO = pd.read_csv('FCPO_Aggregated_PerDay_Data.csv')
-                FCPO['Date'] = pd.to_datetime(FCPO['Date'])
-                FCPO['Date'] = pd.to_datetime(FCPO['Date'], format = "%d/%m/%Y")
-                #FCPO['Date'] = pd.to_datetime(FCPO['Date'], format = "%d/%m/%Y")
-                FCPO.set_index('Date', inplace=True)
-                FCPO['isAvailable']=0
-                FCPO['isAvailable'].iloc[i]=1
-                #FCPO.head(30)
-                # Partition the data to years
-                for yearnow in range(2014, 2017, 1):
-                    dfTrain = FCPO[str(yearnow-3)]
-                    dfGA = FCPO[str(yearnow-2)]
-                    dfTest = FCPO[str(yearnow-1)]
-                    dfTrade = FCPO[str(yearnow)]
-
-	#Load the Rates in a Pandas Frame. All the Data Given can be converted to a CSV And imported into a frame
-
+	def initMarketRates(self):
+		print("In Init")
+		self.FCPO = pd.read_csv('FCPO_Aggregated_PerDay_Data.csv')
+		self.FCPO['Date'] = pd.to_datetime(self.FCPO['Date'])
+		#FCPO['Date'] = pd.to_datetime(FCPO['Date'], format = "%d/%m/%Y")
+		#FCPO.set_index('Date', inplace=True)
+		self.FCPO.set_index('Date')
+		self.FCPO = self.FCPO.sort_index()
+		self.FCPO['isAvailable']=0
+		self.FCPO['isAvailable'].iloc[self.i]=1
+		# Partition the data to years
+		self.train = self.FCPO['2011-01-01':'2013-12-31']
+		self.train_count=self.train.shape[0]
+		self.test  = self.FCPO['2014-01-01':]
+		self.test_count=self.test.shape[0]
+		#form a dictionary pair of dataframes for training and testing separately		
+		#training set  - divided into 6 equal parts aapprox. 
+		self.train_dict = {n: self.train.iloc[n:n+round(self.train_count/6), :] 
+				for n in range(0, len(self.train), round(self.train_count/6))}
+		#testing set  - divided into parts of 5 days
+		self.test_dict = {n: self.test.iloc[n:n+5, :] 
+				for n in range(0, len(self.test), 5)}
+		#train_dict.keys()
+		#len(train_dict)
+		#for key in train_dict.keys
+	
 	def UpdateSysDate(self):
+                print("in update date")
                 self.System_Date += datetime.timedelta(days=1)
-                i = i+1
+                self.i = self.i+1
+                #While (self.System_Date < self.FCPO['Date'].iloc[i].date()):
+                #        self.System_Date += datetime.timedelta(days=1)
+                        #increment the system date till we reach the date for which rates data is available
+	def Update_Sentiment(self):
+    		print("in update sentiment")
                 
-                While (self.System_Date.date() < self.FCPO['Date'].iloc[i].date()):
-                    self.System_Date += datetime.timedelta(days=1)
-                    
-        def UpdateRateAvailability(self):
-                if(self.System_Date.date() < self.FCPO['Date'].iloc[i].date()):
-                    self.FCPO['isAvailable'].iloc[i]=1    
+	def UpdateRateAvailability(self):
+    		if(self.System_Date.date() < self.FCPO['Date'].iloc[self.i].date()):
+    			self.FCPO['isAvailable'].iloc[self.i]=1
 
-        #All the Data Given can be converted to a CSV And imported into a frame
-	#Audit all the Date change and Rate Change info with timestamp into a Separate Pandas Dataframe	
-	def Download(pd ,File_Path):		
-        #just download both audit data frame for trading and Sys Date / Rate Change
-                pd.to_csv(File_Path)
-		#Calculate the total holdings, Cash balance, Value of current holdings , %Profit into a separate dataframe
-		# download all three CSV to the current folder location
+	def BackTesting(self,df):
+                #FOR LOOP TO INCREMENT AND TRADE
+                #---result_bt = ga.GA.CheckTrade(cash_bal,df)
+		self.UpdateSysDate()
+		self.UpdateRateAvailability()
+		Print("In BackTesting")
+                #--self.BackTestingInfo = pd.concat([BackTestingInfo,result_bt])
+		#Execute this to call GA and Fuzzy logic
+	
+	def Training(self,dfTrain):
+		#self.tradingRules = ga.GA.selectPop(popSize, dfTrain,cash_bal) #pass the dataframe for training
+		print("in training")
+		self.Training_Done=1
+				#Flag-off that one round of training is completed and be prepared for back-testing
+		if self.Training_Done==1:
+			self.trading_required = 1
 
-################################################################# 2. Helper Functions (End) ##################################################################################
+	def Trading(self):
+		self.result_trade = ga.GA.CheckTrade(cash_bal,df)
+		self.UpdateSysDate()
+		self.UpdateRateAvailability()
+		self.TradingInfo = pd.concat([self.TradingInfo,self.result_trade])                
 
-################################################################# 3.GA / Fuzzy Implementations for Training and Backtesting ( Start) #########################################
-
-        def BackTesting(self,No_Of_Days):		
-                #Utilise The Set of Selected Rules and the Incoming Rates to decide buy/sell/hold
-		#update the Audit Pandas Data Frame for each execution
-		#value Set - 
-		#No_Of_Days = -60 since The Trading is carried out with previous cycle date
-
-	#Execute this to call GA and Fuzzy logic
-	def Training(self,StartDate,EndDate,NO-OF-Days):
-                ##fetch a dataframe of the above days to be calculated based on above parameters and pass it to GA which calls fuzzy and returns the best rules
-                dfTrain = self.FCPO['isAvailable']
-		#NO-OF-Days= 60 before Go-Live and NO-OF-Days=1 after Go-Live(Jan 1 , 2014) 
-		#if(System_Date=EndDate)# run the training only on the last day of the batch
-			#call this method to select the top-n best rules for Trading or carry it forward to next generation
-                rules = ga.GA.EvolveGA(popSize, dfTrain) #pass the dataframe for training
-                Training_Done = 1
-                        #Flag-off that one round of training is completed and be prepared for back-testing
-		if (Training_Done= 1)
-			trading_required = 1
-		return rules
-		
-	def Trading(self):		
-                #Utilise The Set of Selected Rules and the Incoming Rates to decide buy/sell/hold
-		#No_Of_Days = 0 since the trading is carried out for current date
-		
-################################################################# 3. GA / Fuzzy Implementations for Training and Backtesting ( End) ##########################################
-
-################################################################# 4.Backtesting and Trading (Start) ##########################################################################		
-        #this is for backtesting from 2011- 2013
 	def BuildTradingSystem(self):		
-        for k in range(len(weights.keys())):
-			self.Training()
+		for key in train_dict.keys:
+			self.Training(train_dict[key])
 			self.BackTesting()
-			UpdateSysDateandRates(self)
-	Self.Download("TrainingReport")
-		
+		self.BackTestingInfo("TrainingReport.csv")	
         #this is for actual trading + Training @ end of every day from 2014- 2016
-	def EarnMoney(self):		
-        #just download both audit data frame for trading and Sys Date / Rate Change
-		#Calculate the total holdings, Cash balance, Value of current holdings , %Profit into a separate dataframe
-		# download all three CSV to the current folder location
-		for k in range(len(weights.keys())):
-			self.SelectRules()
-			self.Trading()
-			UpdateSysDateandRates(self)
-			self.Training()
-	Self.Download("TradingReport")
 
-################################################################# 4.Backtesting and Trading (End) ##########################################################################		
+	def EarnMoney(self):
+		for key in test_dict.keys:
+			for rates in test_dict[key]:
+				self.Trading()
+				self.UpdateSysDate()
+				self.UpdateRateAvailability()
+			self.Training(train_dict[key])
+		self.BackTestingInfo("TrainingReport.csv")
